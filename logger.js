6146fbf4a459367017eb068c0e6c36b1ec69383d
obidkhon <obid.sharipov.80@mail.ru>
2020-12-18 09:26:10 +0500

    module

---
+++ logger.js
@@ -0,0 +1,6 @@
+const url='http://192.168.1.1/logger';
+function log (message){
+//  to do buyerda messagni urlda yuboriladtn
+console.log(message)
+}
+module.exports=log
---
+++ nod.js
@@ -0,0 +1,14 @@
+const log=require('./logger')
+
+console.log(log)
+/*
+function great (name){
+    console.log(window)node
+}
+great("Obid")
+
+setTimeout()
+clearInterval
+clearTimeout
+setInterval
+*/
